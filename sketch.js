9962fd8e03fc1f941623c91de076ba2f68272294
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-06-11 20:11:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,38 +74,7 @@ function draw() {
      plinkos[i].display();  
   }
  
-    if(ball!=null)
-    {
-       ball.display();
-        
-        if (ball.body.position.y>760)
-        {
-              if (ball.body.position.x < 300) 
-              {
-                  score=score+500;      
-                  ball=null;
-                  if ( count>= 5) gameState ="end";                          
-              }
-
-
-              else if (ball.body.position.x < 600 && ball.body.position.x > 301 ) 
-              {
-                    score = score + 100;
-                    ball=null;
-                    if ( count>= 5) gameState ="end";
-
-              }
-              else if (ball.body.position.x < 900 && ball.body.position.x > 601 )
-              {
-                    score = score + 200;
-                    ball=null;
-                    if ( count>= 5)  gameState ="end";
-
-              }      
-              
-        }
-  
-      }
+    
 
    for (var k = 0; k < divisions.length; k++) 
    {
